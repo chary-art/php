bd8a656221c0e10563fe56fe8104494a4864664d
chary-art <b_chary@mail.ru>
2026-01-26 17:46:48 +0500

    CRUD in aura.sql

--- app/QueryBuilder.php
+++ app/QueryBuilder.php
@@ -1,22 +1,79 @@
 <?php
 
 namespace App;
+
 use Aura\SqlQuery\QueryFactory;
 use PDO;
 
 class QueryBuilder
 {
+    private $pdo;
+    private $queryFactory;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=app3;charset=utf8', 'root', '');
+        $this->queryFactory = new QueryFactory('mysql');
+    }
+
     public function getAll($table)
     {
-        $queryFactory = new QueryFactory('mysql');
-        $select = $queryFactory->newSelect();
+
+        $select = $this->queryFactory->newSelect();
         $select->cols(['*'])
             ->from('posts');
-        $pdo = new PDO('mysql:host=localhost;dbname=app3;charset=utf8', 'root', '');
-        $sth = $pdo->prepare($select->getStatement());
+        $sth = $this->pdo->prepare($select->getStatement());
         $sth->execute($select->getBindValues());
         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
         var_dump($result);
+    }
+
+    public function findOne($data, $table)
+    {
+        $select = $this->queryFactory->newSelect();
+        $select->cols([
+            'title' => $data,
+        ])
+        ->from($table)           // table name
+        ->where("title LIKE :$data");
+        $sth = $this->pdo->prepare($select->getStatement());
+        $sth->execute($select->getBindValues());
+    }
+
+    public function insert($data, $table)
+    {
+        $insert = $this->queryFactory->newInsert();
+        $insert
+            ->into($table)
+            ->cols($data);
+        $sth = $this->pdo->prepare($insert->getStatement());
+        $sth->execute($insert->getBindValues());
+    }
+
+    public function update($data, $id, $table)
+    {
+        $update = $this->queryFactory->newUpdate();
+
+        $update
+            ->table($table)                  // update this table
+            ->cols($data)
+            ->where('id = :id')      // bind this value to the condition
+            ->bindValue('id', $id);
+
+        $sth = $this->pdo->prepare($update->getStatement());
+        $sth->execute($update->getBindValues());
+    }
+
+    public function delete($id, $table)
+    {
+        $delete = $this->queryFactory->newDelete();
+
+        $delete
+            ->from($table)                   // FROM this table
+            ->where('id = :id')           // AND WHERE these conditions
+            ->bindValue('id', $id);   // bind one value to a placeholder
+        $sth = $this->pdo->prepare($delete->getStatement());
+        $sth->execute($delete->getBindValues());
     }
 }
 
--- app/controllers/homepage.php
+++ app/controllers/homepage.php
@@ -3,4 +3,17 @@
 use App\QueryBuilder;
 
 $db = new QueryBuilder();
-$db->getAll('posts');
+//$db->getAll('posts');
+
+//$db->insert([
+//    'title' => 'neo'
+//], 'posts');
+
+//$db->update([
+//    'id' => 2,
+//    'title' => '2new post from QueryFactory package2'
+//], 2, 'posts');
+
+//$db->delete( 2, 'posts' );
+
+$db->findOne('walli', 'posts');
